f88a65c86bc948c4fdb14384afc54790524e0ab7
realayna <ayna1204@gmail.com>
2023-09-06 13:07:16 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include "tictactoe.h"
 
 int main()
 {
-    
+    std::vector<std::vector<char>> board(BOARD_SIZE, std::vector<char>(BOARD_SIZE, EMPTY_CELL));
+    bool playerTurn = true;
 }
