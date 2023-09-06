80fb9647cb5d2e973b6cd32d601c471d76da6a54
realayna <ayna1204@gmail.com>
2023-09-06 12:34:27 +0200

    added display borard

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,3 +3,17 @@
 #include <vector>
 #include <climits>
 
+void displayBoard(const std::vector<std::vector<char>>& board) {
+    for (int i = 0; i < BOARD_SIZE; ++i) {
+        for (int j = 0; j < BOARD_SIZE; ++j) {
+            std::cout << board[i][j];
+            if (j < BOARD_SIZE - 1)
+                std::cout << " | ";
+        }
+        std::cout << std::endl;
+        if (i < BOARD_SIZE - 1) {
+            std::cout << "---------" << std::endl;
+        }
+    }
+    std::cout << std::endl;
+}
