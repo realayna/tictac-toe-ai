205a2bf5c2d882942c35514ca31e44b8fe208cbc
realayna <ayna1204@gmail.com>
2023-09-06 14:26:08 +0200

    fixed win

--- main.cpp
+++ main.cpp
@@ -10,8 +10,50 @@ int main()
     while (true)
     {
         displayBoard(board);
+        if (playerTurn)
+        {
+            int row, col;
+            std::cout << "Enter your move (row and column): ";
+            std::cin >> row >> col;
 
+            row -= 1;
+            col -= 1;
+            if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE &&
+                board[row][col] == EMPTY_CELL)
+            {
+                board[row][col] = PLAYER_O;
+                playerTurn = false;
+            }
+            else
+            {
+                std::cout << "Invalid move. Try again." << std::endl;
+                continue;
+            }
+        }
+        else
+        {
+            Move bestMove = findBestMove(board);
+            board[bestMove.row][bestMove.col] = PLAYER_X;
+            playerTurn = true;
+        }
+
+        if (isWinner(board, PLAYER_X))
+        {
+            displayBoard(board);
+            std::cout << "AI win!" << std::endl;
+            break;
+        }
+        else if (isWinner(board, PLAYER_O))
+        {
+            displayBoard(board);
+            std::cout << "You win!" << std::endl;
+            break;
+        }
+        else if (isBoardFull(board))
+        {
+            displayBoard(board);
+            std::cout << "It's a draw!" << std::endl;
+            break;
+        }
     }
-    return 0;
-    
 }
