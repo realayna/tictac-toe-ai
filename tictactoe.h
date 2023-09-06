8fe2b806a3abc663d0357f45d61b39d0d011df10
realayna <ayna1204@gmail.com>
2023-09-06 12:32:30 +0200

    added functuons

--- tictactoe.h
+++ tictactoe.h
@@ -9,7 +9,17 @@ const char EMPTY_CELL = ' ';
 const char PLAYER_X = 'X';
 const char PLAYER_O = 'O';
 
+struct Move{
+    int row; int col;
 
+};
+
+void displayBoard(const std::vector<std::vector<char>>& board);
+bool isBoardFull(const std::vector<std::vector<char>>& board);
+bool isWinner(const std::vector<std::vector<char>>& board, char player);
+int evaluateBoard(const std::vector<std::vector<char>>& board);
+int minimax(std::vector<std::vector<char>>& board, int depth, bool isMaximizing);
+Move findBestMove(std::vector<std::vector<char>>& board);
 
 #endif // TICTACTOE_H
 
